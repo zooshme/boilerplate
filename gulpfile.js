4137e4760e6542987345850fd12f8bd4de1347ad
Ovidiu Spatacian-Tarnu <github@zoosh.me>
2014-07-02 22:20:05 +0100

    removed vender and bower and added connect server

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,37 @@ var gulp = require('gulp');
 var stylus = require('gulp-stylus');
 var nib = require('nib');
 var jade = require('gulp-jade');
+var connect = require('gulp-connect');
+var changed = require('gulp-changed');
+
+gulp.task('connect', function() {
+	connect.server({
+		root: './public',
+		livereload: true,
+		port: 4141
+	});
+});
 
 gulp.task('stylus', function() {
 	gulp.src('./compile/stylus/styles.styl')
 		.pipe(stylus({
 			use: [nib()]
 		}))
-		.pipe(gulp.dest('./css'));
+		.pipe(gulp.dest('./public/css'))
+		.pipe(connect.reload();
 });
 
 gulp.task('jade', function() {
 	gulp.src(['./compile/jade/**/*.jade', '!./compile/jade/partials/*.jade'])
+		.pipe(changed('./'))
 		.pipe(jade({
 			pretty: true
 		}))
-		.pipe(gulp.dest('./'));
+		.pipe(gulp.dest('./public'))
+		.pipe(connect.reload();
 });
 
-gulp.task('default', ['stylus', 'jade']);
+gulp.task('default', ['stylus', 'jade', 'connect']);
 
 gulp.watch('./compile/stylus/**/*.styl', ['stylus']);
 gulp.watch('./compile/jade/**/*.jade', ['jade']);
